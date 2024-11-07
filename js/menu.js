0008d0181d7e9d68ab61f7ec5079a21baa3156e9
32Valik92 <strukalo.valentyn@gmail.com>
2024-11-07 11:10:02 +0200

    header

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    document.getElementById('side-menu').style.width = '295px';
+    document.querySelector('.menu-icon').style.display = 'none';
+}
+
+function closeMenu() {
+    document.getElementById('side-menu').style.width = '0';
+    document.querySelector('.menu-icon').style.display = 'block';
+}
